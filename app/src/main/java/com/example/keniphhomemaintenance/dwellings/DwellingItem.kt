4b470a806ce7a417e72cb6e829674e81be0e81a9
NickPerozzi <53713045+NickPerozzi@users.noreply.github.com>
2021-12-29 15:26:34 -0600

    deleted one comment in DwellingItem.kt

--- app/src/main/java/com/example/keniphhomemaintenance/dwellings/DwellingItem.kt
+++ app/src/main/java/com/example/keniphhomemaintenance/dwellings/DwellingItem.kt
@@ -1,7 +1,6 @@
 package com.example.keniphhomemaintenance.dwellings
 
 import androidx.compose.foundation.layout.Column
-// import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
